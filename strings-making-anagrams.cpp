8e517d2bce19b28cf7b4dc3c967d11875392ae91
Kausthub Raj Jadhav <kjadhav@uci.edu>
2017-01-05 22:03:12 -0800

    Add basic structure

--- strings-making-anagrams.cpp
+++ strings-making-anagrams.cpp
@@ -1,1 +1,37 @@
-.
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+int number_needed(string a, string b) {
+   
+}
+
+int main(){
+    string a;
+    cin >> a;
+    string b;
+    cin >> b;
+    cout << number_needed(a, b) << endl;
+    return 0;
+}
