b5bc24f68492fdaea0034ca57bba2d206346f7b4
Kausthub Raj Jadhav <kjadhav@uci.edu>
2017-01-25 13:13:19 -0800

    Start Logic 2

--- strings-making-anagrams.cpp
+++ strings-making-anagrams.cpp
@@ -26,50 +26,10 @@
 
 using namespace std;
 
-//Logic 1/ Approach 1 produces runtime error
+//Logic 2
 int number_needed(string a, string b) {
-   // Sort strings
-   // Delete from end till strings match
-   
-   //Sort strings a and b
-   sort(a.begin(), a.end());
-   sort(b.begin(), b.end());
-
-    //Search charecters
-    int result = a.find("d");
-    cout <<" search_result = " << result;
-   
-    // Declare index
-    int a_i = 0;
-    int b_i = 0;
-   
-   // Logic 1
-    while ((a.length() != 0 && b.length() != 0)){
-        if (b.find(a[a_i]) == -1){
-           number_needed ++;
-           //Delete charecters
-           a.erase(remove(a.begin(), a.end(), a[a_i]), a.end());
-        }
-
-        else {
-            a_i ++;
-        }
-    
-        if (a.find(b[b_i]) == -1){
-           number_needed ++;
-           b.erase(remove(b.begin(), b.end(), b[b_i]), b.end());
-        }
-
-        else {
-            b_i ++;
-        }
-        
-        if(a.compare(b) == 0){
-            break;
-        }
-     }
-       
-    return number_needed;
+    int number_needed = 0;
+    vector<int> freq (26, 0); // Create a vector with 26 values all set to 0
 }
 
 int main(){
