1b0b7dfa83b26f5eefa82173c261a2fe0fbe4459
Kausthub Raj Jadhav <kjadhav@uci.edu>
2017-01-25 13:15:59 -0800

    Working solution :tada:

--- strings-making-anagrams.cpp
+++ strings-making-anagrams.cpp
@@ -30,6 +30,16 @@ using namespace std;
 int number_needed(string a, string b) {
     int number_needed = 0;
     vector<int> freq (26, 0); // Create a vector with 26 values all set to 0
+    
+    //Complexity: O(N)
+    for(auto ch : a) ++freq[ch - 'a']; //ch - 'a' evaluates -> ascii code(ch) - 97
+    for(auto ch : b) --freq[ch - 'a']; // This loops accounts for charecters that exists in both strings
+    
+    //The sum of absolute values in freq evalutates to the number needed 
+    for(auto fq : freq)number_needed += abs(fq);
+        
+    return number_needed;
+    
 }
 
 int main(){
