828293a47322b298dc78afbe8c7e5f93feb0a80d
Kausthub Raj Jadhav <kjadhav@uci.edu>
2017-01-26 15:17:34 -0800

    Add basic structure

--- hash-tables-ransom-note.cpp
+++ hash-tables-ransom-note.cpp
@@ -21,3 +21,27 @@
 #include <algorithm>
 #include <unordered_map>
 
+using namespace std;
+
+bool ransom_note(vector<string> magazine, vector<string> ransom) {
+    
+}
+
+int main(){
+    int m;
+    int n;
+    cin >> m >> n;
+    vector<string> magazine(m);
+    for(int magazine_i = 0;magazine_i < m;magazine_i++){
+       cin >> magazine[magazine_i];
+    }
+    vector<string> ransom(n);
+    for(int ransom_i = 0;ransom_i < n;ransom_i++){
+       cin >> ransom[ransom_i];
+    }
+    if(ransom_note(magazine, ransom))
+        cout << "Yes\n";
+    else
+        cout << "No\n";
+    return 0;
+}
