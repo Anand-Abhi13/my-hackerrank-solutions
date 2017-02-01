22093d2ac7b342761a66adbf8d93be204b8e4bd3
A <kjadhav@uci.edu>
2017-02-01 08:46:20 -0800

    Add logic 1

--- linked-list-detect-a-cycle.cpp
+++ linked-list-detect-a-cycle.cpp
@@ -1,17 +1,3 @@
-
- cycle in a linked list. Note that the head pointer may be 'NULL' if the list is empty.
-
-A Node is defined as: 
-    struct Node {
-        int data;
-        struct Node* next;
-    }
-*/
-
-bool has_cycle(Node* head) {
-    // Complete this function
-    // Do not write the main method
-}
 /*
 Detect a cycle in a linked list. Note that the head pointer may be 'NULL' if the list is empty.
 
@@ -22,22 +8,11 @@ A Node is defined as:
     }
 */
 
+// Logic 1
 bool has_cycle(Node* head) {
-    // Complete this function
-    // Do not write the main method
+    //save data in hastable
+    //If encountered null; return false
+    //If encountered same memory location twice; return true
+     
+    return true;
 }
-/*
-Detect a cycle in a linked list. Note that the head pointer may be 'NULL' if the list is empty.
-
-A Node is defined as: 
-    struct Node {
-        int data;
-        struct Node* next;
-    }
-*/
-
-bool has_cycle(Node* head) {
-    // Complete this function
-    // Do not write the main method
-}
-
